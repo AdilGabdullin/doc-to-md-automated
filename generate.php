a69e705fb0ef53013334b8f99d44d7aaa1349437
Adil Gabdullin <Izenbard@gmail.com>
2018-02-21 12:23:52 +0600

    fix inviseble columns, italic, list in one cell

--- generate.php
+++ generate.php
@@ -1,29 +1,57 @@
 <?php
 require_once 'TextTable.php';
 
-function generateTable($fileName, $columnsNum)
+function automate(string $fileName): string
 {
     $data = file_get_contents($fileName);
-    $lines = explode("\r\n", $data);
-    $columns = array_slice($lines, 0, $columnsNum);
-    $rows = [];
-    $stop = count($lines) / $columnsNum - 1;
-    for ($i = 1; $i < $stop; $i++) {
-        $rows[] = array_slice($lines, $i * $columnsNum, $columnsNum);
+    $blocks = explode("\r\n\r\n", $data);
+    $result = '';
+    foreach ($blocks as $block) {
+        if ($block[0] === '#') {
+            $result .= $block . PHP_EOL;
+        } else {
+            $result .= generateTable($block);
+        }
     }
-    $t = new TextTable($columns, $rows);
-    return $t->render() . PHP_EOL;
+    return $result;
 }
 
-$result = '# Metric descriptions for Workplace Analytics' . PHP_EOL;
-$result .= '## Person metrics' . PHP_EOL;
-$result .= generateTable('input/person metrics.txt', 3);
-$result .= '## Meeting metrics'. PHP_EOL;
-$result .= generateTable('input/meeting metrics.txt', 3);
-$result .= '## Group metrics'. PHP_EOL;
-$result .= generateTable('input/group metrics.txt', 3);
-$result .= '# Glossary for Workplace Analytics'. PHP_EOL;
-$result .= generateTable('input/Glossary for Workplace Analytics.txt', 2);
+function generateTable(string $data): string
+{
+    $lines = explode("\r\n", $data);
+    $columnsNum = (int)$lines[0];
+    $rowsNum = (count($lines) - 1) / $columnsNum;
 
+    $tableHead = '|';
+    for ($j = 0; $j < $columnsNum; $j++) {
+        $tableHead .= $lines[$j + 1] . '|';
+    }
+    $result = $tableHead . PHP_EOL;
+    $result .= preg_replace('/[^|]/', '-', $tableHead) . PHP_EOL;
+
+
+    for ($i = 1; $i < $rowsNum; $i++) {
+        $row = '|';
+        for ($j = 0; $j < $columnsNum; $j++) {
+            $row .= $lines[$j + 1 + $i * $columnsNum] . '|';
+        }
+        $result .= $row . PHP_EOL;
+    }
+    return $result;
+}
+
+$result = automate('input.txt');
 echo $result;
 file_put_contents('Metric descriptions and glossary.md', $result);
+//$result = '# Metric descriptions for Workplace Analytics' . PHP_EOL;
+//$result .= '## Person metrics' . PHP_EOL;
+//$result .= generateTable('input/person metrics.txt', 3);
+//$result .= '## Meeting metrics'. PHP_EOL;
+//$result .= generateTable('input/meeting metrics.txt', 3);
+//$result .= '## Group metrics'. PHP_EOL;
+//$result .= generateTable('input/group metrics.txt', 3);
+//$result .= '# Glossary for Workplace Analytics'. PHP_EOL;
+//$result .= generateTable('input/Glossary for Workplace Analytics.txt', 2);
+//
+//echo $result;
+//file_put_contents('Metric descriptions and glossary.md', $result);
